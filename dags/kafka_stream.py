2cf24021eb7a17156f3dcb4db717288db7019d06
root <root@brian>
2024-09-09 16:55:23 -0300

    apache airflow and kafka setup with postgreSQL DB

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-default_args = {"owner": "brian_song", "start_date": datetime(2024, 9, 7, 7, 00)}
+default_args = {"owner": "airscholar", "start_date": datetime(2023, 9, 3, 10, 00)}
 
 
 def get_data():
@@ -19,13 +19,13 @@ def format_data(res):
     data = {}
     location = res["location"]
     data["first_name"] = res["name"]["first"]
-    data["last name"] = res["name"]["last"]
+    data["last_name"] = res["name"]["last"]
     data["gender"] = res["gender"]
     data["address"] = (
-        f"{str(location['street']['number'])} {location['street']['name']} "
-        f"{location['city']}, {location['state'], {location['country']}}"
+        f"{str(location['street']['number'])} {location['street']['name']}, "
+        f"{location['city']}, {location['state']}, {location['country']}"
     )
-    data["postcode"] = location["postcode"]
+    data["post_code"] = location["postcode"]
     data["email"] = res["email"]
     data["username"] = res["login"]["username"]
     data["dob"] = res["dob"]["date"]
@@ -38,20 +38,34 @@ def format_data(res):
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
+    import logging
 
-    res = get_data()
-    res = format_data(res)
-    print(json.dumps(res, indent=3))
+    producer = KafkaProducer(bootstrap_servers=["broker:29092"], max_block_ms=5000)
+    curr_time = time.time()
+
+    while True:
+        if time.time() > curr_time + 60:  # 1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+
+            producer.send("users_created", json.dumps(res).encode("utf-8"))
+
+        except Exception as e:
+            logging.error(f"An error occured: {e}")
+            continue
 
 
-"""with DAG('user_autoamtion',
-         default_args=default_args,
-         schedule_interval='@daily',
-         catchup=False) as dag:
-    
+with DAG(
+    "user_automation",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+
     streaming_task = PythonOperator(
-        task_id='stream_data_from_api',
-        python_callable=stream_data
-    )"""
-
-stream_data()
+        task_id="stream_data_from_api", python_callable=stream_data
+    )
